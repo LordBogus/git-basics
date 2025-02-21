d963706c8914759d0e9894ca520a7378d2abd6c5
Edgar <edgarwow1991@gmail.com>
2025-02-20 21:06:50 -0600

    added 69 arrays

--- homework/js-push.exercises.js
+++ homework/js-push.exercises.js
@@ -1,11 +1,6 @@
 // add onto end array next number
 const arr = [1, 2, 3, 4, 5]
 
-// expected output : [1, 2, 3, 4, 5, 6]
+// expected output : [1, 2, 3, 4, 5, 6, 69]
 arr.push(6, 69);
 console.log(arr);
-
-//expected output : [1, 2, 3, 4, 5, 6, 69]
-
-
-
