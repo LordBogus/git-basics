dfdf3084f1dbd85317caffff9567c8972eeb928c
Edgar <edgarwow1991@gmail.com>
2025-02-20 21:02:26 -0600

    homework commit arrays

---
+++ homework/js-push.exercises.js
@@ -0,0 +1,11 @@
+// add onto end array next number
+const arr = [1, 2, 3, 4, 5]
+
+// expected output : [1, 2, 3, 4, 5, 6]
+arr.push(6, 69);
+console.log(arr);
+
+//expected output : [1, 2, 3, 4, 5, 6, 69]
+
+
+
